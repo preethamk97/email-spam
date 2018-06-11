e18a9405a9d7415e1c51f6b0901c2fefa65a9729
preethamk97 <preethamk1997@gmail.com>
2018-06-11 18:07:04 +0530

    email spamming

---
+++ emailspam.py
@@ -0,0 +1,98 @@
+#Spam mail detection using Random Forest Classifier.
+
+def make_Dictionary(train_dir):
+    emails = [os.path.join(train_dir,f) for f in sorted(os.listdir(train_dir))]    
+    all_words = []       
+    for mail in emails:    
+        with open(mail) as m:
+            for i,line in enumerate(m):
+                if i == 2:  #Body of email is only 3rd line of text file
+                    words = line.split()
+                    all_words += words
+    
+    dictionary = Counter(all_words)
+    # Paste code for non-word removal here(code snippet is given below) 
+    list_to_remove = list(dictionary)
+    for item in list_to_remove:
+        if item.isalpha() == False: 
+            del dictionary[item]
+        elif len(item) == 1:
+            del dictionary[item]
+    dictionary = dictionary.most_common(3000)
+    return dictionary
+
+def extract_features(mail_dir): 
+    files = [os.path.join(mail_dir,fi) for fi in sorted(os.listdir(mail_dir))]
+    features_matrix = np.zeros((len(files),3000))
+    docID = 0;
+    for fil in files:
+      with open(fil) as fi:
+        for i,line in enumerate(fi):
+          if i == 2:
+            words = line.split()
+            for word in words:
+              wordID = 0
+              for i,d in enumerate(dictionary):
+                if d[0] == word:
+                  wordID = i
+                  features_matrix[docID,wordID] = words.count(word)
+        docID = docID + 1     
+    return features_matrix
+
+import os
+import numpy as np
+from collections import Counter
+from sklearn.naive_bayes import MultinomialNB, GaussianNB, BernoulliNB
+from sklearn.svm import SVC, NuSVC, LinearSVC
+from sklearn.metrics import confusion_matrix 
+import matplotlib.pyplot as plt
+# Create a dictionary of words with its frequency
+
+train_dir = 'train-mails'
+dictionary = make_Dictionary(train_dir)
+
+# Prepare feature vectors per training mail and its labels
+
+train_labels = np.zeros(702)
+train_labels[351:701] = 1
+train_matrix = extract_features(train_dir)
+
+# Training SVM and Naive bayes classifier
+
+model1 = MultinomialNB()
+model2 = LinearSVC()
+model1.fit(train_matrix,train_labels)
+model2.fit(train_matrix,train_labels)
+
+# Test the unseen mails for Spam
+test_dir = 'test-mails'
+test_matrix = extract_features(test_dir)
+test_labels = np.zeros(260)
+test_labels[130:260] = 1
+result1 = model1.predict(test_matrix)
+result2 = model2.predict(test_matrix)
+print (confusion_matrix(test_labels,result1))
+print (confusion_matrix(test_labels,result2))
+
+from sklearn.ensemble import RandomForestClassifier
+model3=RandomForestClassifier(n_estimators=90,criterion='entropy',random_state=0)
+model3.fit(train_matrix,train_labels)
+result3 = model3.predict(test_matrix)
+print (confusion_matrix(test_labels,result3))
+
+"""from matplotlib.colors import ListedColormap
+X_set, y_set = test_matrix, test_labels
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, model3.predict(np.array([X1.ravel(), X2.ravel()])),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('Random Forest Classification (Test set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()"""
